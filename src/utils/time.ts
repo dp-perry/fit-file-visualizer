25c6f1695b0bfd75b2ad9b03784a9736b8693548
dperry <daan.perry@proton.me>
2025-04-28 22:44:23 -0600

    Create util to parse time duration nicer, sub 1 hour shows minutes only more than 1 hour will show ""# hr # minutes"

---
+++ src/utils/time.ts
@@ -0,0 +1,15 @@
+export function formatTime(seconds: number): string {
+  const minutes = parseInt((seconds / 60).toFixed(0))
+  if (minutes < 60) {
+    return `${minutes} min`;
+  }
+
+  const hours = Math.floor(minutes / 60);
+  const remainingMinutes = minutes % 60;
+
+  if (remainingMinutes === 0) {
+    return `${hours} hr`;
+  }
+
+  return `${hours} hr ${remainingMinutes} min`;
+}
